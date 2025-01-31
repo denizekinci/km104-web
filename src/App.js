0fd050a5f468503e8144e508615dd70852cf0def
Deniz Ekinci <deniz.ekinci.dev@gmail.com>
2025-01-31 21:05:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+      <h1>Deployment Demo</h1>
+      <p>
+        Bu uygulama hem Vercel'de hem de sunucuya build alınıp çalıştırılacak
+        şekilde hazırlanmıştır.
+      </p>
+      <p>Bu bir React Single Page Application (SPA) örneğidir.</p>
     </div>
   );
 }
-
-export default App;
